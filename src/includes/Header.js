364fa145ed5f344c6267870f97ebad820a1a4504
arthurjf <arthur.310100@hotmail.com>
2023-12-30 18:54:24 -0300

    Navbar show active route

--- src/includes/Header.js
+++ src/includes/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import '../css/style.css';
+import '../css/navbar.css';
 
 function Header() {
     return (
         <header>
             <nav className="navbar navbar-expand-lg fixed-top">
                 <div className="container">
-                    <Link className="navbar-brand" to="/about">Arthur Fernandes</Link>
+                    <NavLink className="navbar-brand" to="/about">Arthur Fernandes</NavLink>
                     <div className="d-md-none">
-                        <button className="btn btn-custom" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        <button className="btn btn-nav" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             Menu
                         </button>
@@ -16,19 +18,22 @@ function Header() {
                     <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link className="nav-link" to="/about">About Me</Link>
+                                <NavLink className="nav-link first-item" activeClassName="active" exact to="/about">About Me</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/skills">Skills</Link>
+                                <NavLink className="nav-link middle-item" activeClassName="active" to="/skills">Skills</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/projects/transitcourse">Projects</Link>
+                                <NavLink className="nav-link middle-item" activeClassName="active" to="/projects/transitcourse">Projects</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/privacy">Privacy Policy</Link>
+                                <NavLink className="nav-link last-item" activeClassName="active" to="/privacy">Privacy Policy</NavLink>
                             </li>
                         </ul>
                     </div>
+                    <a className="btn btn-custom" href="./res/CV.pdf" target="_blank" rel="noopener noreferrer">
+                        <i className="bi bi-file-earmark-arrow-down-fill"></i> View Résumé
+                    </a>
                 </div>
             </nav>
         </header>
