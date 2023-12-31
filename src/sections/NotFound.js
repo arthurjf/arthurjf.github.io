943a608faecf1b4cf92495df2ff616ccb2fc1b44
arthurjf <arthur.310100@hotmail.com>
2023-12-30 21:06:20 -0300

    Better 404 page

--- src/sections/NotFound.js
+++ src/sections/NotFound.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../includes/Header';
+import '../css/notfound.css';
 
 const NotFound = () => {
     return (
         <div>
             <Header />
-            <div className="container content">
-                <h2>Sorry =(</h2>
-                <p>That page cannot be found.</p>
-                <Link className="btn btn-primary" to="/">Back to the homepage...</Link>
+            <div className="container content not-found">
+                <h1 className="text-center title">404</h1>
+                <h2 className="text-center subtitle">Not Found :(</h2>
+                <p className="text-center">Sorry, we couldn't find this page. But don't worry, you can find plenty of other things on our <Link className="link" to="/">homepage</Link>.</p>
             </div>
         </div>
     );
