85058efda80e05689e2f69e8fe7bed12e365617d
arthurjf <arthur.310100@hotmail.com>
2024-01-05 12:03:17 -0300

    Ongoing projects display

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../includes/Header';
 import ProjectCard from '../includes/ProjectCard';
 import '../css/projects.css';
+import '../css/navbar-projects.css';
 
 const Projects = () => {
     const projectData = [
@@ -29,7 +30,28 @@ const Projects = () => {
             date: 'jul 2023 - dez 2023',
             highlightedIcons: ['googleplay', 'android'],
         },
+        {
+            imageUrl: './res/img/projects/werewolfdungeon.png',
+            title: 'Werewolf Dungeon',
+            description: "I'm developing a cooperative horror game.",
+            skills: ['Unity', 'C# (Unity)', 'Git'],
+            date: 'jan 2024 - the moment',
+            highlightedIcons: ['googleplay', 'android'],
+        },
     ];
+
+    const publishedProjects = [projectData[0], projectData[1], projectData[2]];
+    const ongoingProjects = [projectData[3]];
+
+    const renderProjects = (projects) => (
+        <div className="row">
+            {projects.map((project, index) => (
+                <div key={index} className="col-lg-4 mb-4">
+                    <ProjectCard {...project} />
+                </div>
+            ))}
+        </div>
+    );
 
     return (
         <div>
@@ -38,14 +60,15 @@ const Projects = () => {
                 <h1>Projects</h1>
                 <p>These are the projects I have worked on or am currently working on.</p>
                 <hr />
-                <h2>Published Projects</h2>
-                <div className="row">
-                    {projectData.map((project, index) => (
-                        <div key={index} className="col-lg-4 mb-4">
-                            <ProjectCard {...project} />
-                        </div>
-                    ))}
-                </div>
+                <section>
+                    <h2 className="mt-5">Published Projects</h2>
+                    {renderProjects(publishedProjects)}
+                </section>
+                <hr />
+                <section>
+                    <h2 className="mt-5">Ongoing Projects 🔨</h2>
+                    {renderProjects(ongoingProjects)}
+                </section>
             </div>
         </div>
     );
