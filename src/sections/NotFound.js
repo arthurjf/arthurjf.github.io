e9064497646f36300839970156c7deb12c49a1da
Arthur J F <arthur.310100@gmail.com>
2024-01-13 01:56:19 -0300

    Not found page parcial translation

--- src/sections/NotFound.js
+++ src/sections/NotFound.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/notfound.css';
+import { useTranslation } from 'react-i18next';
 
 const NotFound = () => {
+    const { t } = useTranslation("global");
+
     return (
         <div>
             <div className="container content not-found">
                 <h1 className="text-center title">404</h1>
-                <h2 className="text-center subtitle">Not Found :(</h2>
-                <p className="text-center">Sorry, we couldn't find this page. But don't worry, you can find plenty of other things on our <Link className="text-link" to="/">homepage</Link>.</p>
+                <h2 className="text-center subtitle">{t("notFound.subtitle")}</h2>
+                <p className="text-center pt-4">Sorry, we couldn't find this page. But don't worry, you can find plenty of other things on our <Link className="text-link" to="/">homepage</Link>.</p>
             </div>
         </div>
     );
