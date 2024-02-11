739e025be07c20a88240c694f3f8124609afcb56
Arthur J F <arthur.310100@gmail.com>
2024-02-11 15:35:06 -0300

    Image loading

---
+++ src/includes/PlaceholderImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/skeleton-project-image.css';
+
+const PlaceholderImage = () => {
+    return (
+        <div className="skeleton-img p-3"
+        ></div>
+    );
+};
+
+export default PlaceholderImage;
--- src/includes/ProjectCard.js
+++ src/includes/ProjectCard.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PlaceholderImage from './PlaceholderImage'; // Importe o componente de placeholder
 import '../css/style-skills.css';
 
 const storeIcons = {
@@ -11,9 +12,23 @@ const storeIcons = {
 };
 
 const ProjectCard = ({ imageUrl, title, description, date, highlightedIcons, linkurl }) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const image = new Image();
+        image.src = imageUrl;
+        image.onload = () => {
+            setIsLoading(false);
+        };
+    }, [imageUrl]);
+
     return (
         <Link to={"/projects/" + linkurl} className="card mt-4 project">
-            <img src={imageUrl} className="card-img-top p-3" alt="Project" />
+            {isLoading ? (
+                <PlaceholderImage alt="Project" />
+            ) : (
+                <img src={imageUrl} className="card-img-top p-3" alt="Project" />
+            )}
             <div className="card-body">
                 <h5 className="card-title text-primary">{title}</h5>
                 <p className="card-text date">{date}</p>
